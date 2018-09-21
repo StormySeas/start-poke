a9188c4cf28e7359393afacf8ba556338937c9de
Julian <julianboyd0@gmail.com>
2018-09-21 13:23:03 -0500

    question 6?

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,21 +1,72 @@
-// The objective of our game is to click on as many blue squares as possible during the alloted time
-// User Stories/game logic
-// When the user clicks begin the timer should start, and the squares should populate with a random color
-//
-// When the use clicks on a color the color should disapear, and score should be added or subtracted
-//
-// When the round is over the scores round and time should be updated and the user should be able to start again with increased difficulty.
-var colors = ['#ff0000', '#00ff00', '#0000ff']
-var random_Color = colors[Math.floor(Math.random() * colors.length)]
-$('#title').css('color', random_Color)
+let score = 0
+let timer = 10
+let
 $('button').on('click', () => {
   console.log('click works')
+  // When the user clicks the button, populate the squares
   createSquares(30)
+
 })
-function createSquares (numberOfSquares) {
-  for ( let i = 0; i <= numberOfSquares; i++ ) {
-    const $div = $('<div/>')
-    $('.squares').append($div)
+const setInterval = function() => {
+  for (var i = 0; i < ; i++) {
   }
 }
-square.css('background-color')
+// Create a function named createSquares, that takes
+// a parameter of an arbitary number and creates
+// divs according to the number in the square class
+
+const createSquares = (numberOfSquares) => {
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    // creating squares
+    const $div = $('<div/>').on('click', disappearSquares)
+    // this is where the color is applied
+    applyRandomColor($div)
+    // attach to the square class
+    $('.square').append($div)
+  }
+}
+
+const disappearSquares = (e) => {
+  // e is short for event
+  // e.currentTarget gives you the div
+  // you are clicking on
+  const color = $(e.currentTarget).css('background-color')
+
+  checkValidPoke(color)
+
+  $(e.currentTarget).css('opacity', 0)
+}
+
+// lets write a function called (checkValidPoke)
+// that takes that color string
+// and if it is blue lets update a global score variable.
+const checkValidPoke = (colorAsString) => {
+
+
+  if (colorAsString === 'rgb(0, 0, 255)') {
+    // updating the score
+    score++
+  } else {
+    score--
+  }
+  // actuall updating on the DOM
+  $('h1').text('Scoreboard: ' + score)
+}
+// Now we have a bunch of blue squares,
+// but we want our squares to have a random,
+// red, blue, or green color
+// try to write a function to make that happen.
+
+const applyRandomColor = (square) => {
+  const randNum = Math.floor(Math.random() * 3) + 1
+
+  // im assuming square is a jquery object
+  if (randNum === 1) {
+    square.css('background-color', 'red')
+  } else if (randNum === 2) {
+    square.css('background-color', 'blue')
+  } else {
+    square.css('background-color', 'green')
+  }
+}
